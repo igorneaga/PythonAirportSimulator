a9a370377add6e0183d61ab18e144aca18d539e5
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-10-14 21:23:33 -0500

    adding airport simulation function- will remove and add items to the queue

--- flightSimulations.py
+++ flightSimulations.py
@@ -11,18 +11,22 @@ allFlights = []
 departedFlights = []
 
 
-def read_file(flightRequests):
-    allFlights = []
+def read_file(flightRequests):  # Function that will read in the file
+    allFlights = []  # Flight request are held in this
 
     with open(flightRequests, 'rb') as f:
         file = csv.reader(f)
-        _ = next(file)
+        _ = next(file)  # removes cvs header
 
         for row in file:
+            # Converts submission time, request start , and requested duration from strings into integers
             newRow = [row[0], int(row[1]), int(row[2]), int(row[3]), 0, 0]
+
             allFlights.append(newRow)
     return(allFlights)
 
+
+# prints flights that are currently in the queue
 def printQueue(queue, currentTime):
     if len(queue) > 0:
         print("\n=======================================================")
@@ -45,8 +49,15 @@ def printQueue(queue, currentTime):
         print("Currently Departing: No flights are curentlt departing ")
         print("Waiting: ")
 
-    printQueue()
+# This function is going to be removing and adding flights from the queue. Basically like how the airport functions
+def simulate_airport(allFlights):
+    queue = [] # queue used for flight request
+    currentTime = 0
+    queueCount = 0
+
+    # will continue running program if the queue count is less than number of flights
 
 
-
+if __name__ == "__main__":
+    __test__main__()
 
